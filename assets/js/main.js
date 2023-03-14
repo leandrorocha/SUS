f3870cdccc30aefc5e8dd7ba088b8274bf348bf4
leandrobrandao <leandrobrandao@unasus.gov.br>
2023-03-14 11:59:52 -0300

    feat add mod3 aula 2 e ajustes gerais

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,3 @@
-/* Fixa Menu Superior */
-$(window).scroll(function () {
-  if ($(window).scrollTop() + $(window).height() !== $(document).height()) {
-    $(".navbar").addClass("fixed-top");
-  }
-
-  if ($(window).scrollTop() === 0) {
-    $(".navbar").removeClass("fixed-top");
-    $("#row-main").css("padding-top", "0%");
-    $(".navbar").css("box-shadow", "0px 0px 0px rgba(0,0,0,0.5)")
-  } else {
-    $(".navbar").addClass("fixed-top");
-    $("#row-main").css("padding-top", ($('#menuSuperior').css("height")));
-    $(".stepContainer").css("padding-bottom", ($('#menuSuperior').css("height")));
-    $(".navbar").css("box-shadow", "0px 2px 2px rgba(0,0,0,0.5)")
-  }
-});
-
 /*Controla funções no Menu Lateral*/
 $(function () {
   class Accordion {
